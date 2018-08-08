b8a13e95f542e73f6779458590f850417cacc6ca
PengpengWei <lidd3322@mail.ustc.edu.cn>
2018-08-09 03:38:48 +0800

    Runnable P2PSim
    Using Csma on sender side. Using Ipv4GlobalRoutingHelper instead of
    static routing.

---
+++ P2PSim.cc
@@ -0,0 +1,170 @@
+﻿#include <iostream>
+#include <cmath>
+#include <random>
+#include "ns3/netanim-module.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/internet-apps-module.h"
+#include "ns3/flow-monitor-helper.h"
+#include "ns3/traffic-control-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/queue.h"
+
+using namespace std;
+using namespace ns3;
+
+#define PORT 10000
+
+// Tunable parameters:
+#define LINK_CAPACITY 1
+#define TRAFFIC_INTENSITY 1
+#define TRAFFIC_VAR 0.1
+#define MAX_SIM_TIME 1.5
+#define NOISE_RESOLUTION 0.74
+ 
+
+NS_LOG_COMPONENT_DEFINE("P2PSim");
+
+int main(void) {
+	Time::SetResolution(Time::NS);
+	
+	TrafficControlHelper tch;
+
+	///
+	/// Build network
+	///
+
+	// Create routers and end systems
+	NodeContainer senderNodes, routerNodes, receiverNode;
+	int num_of_sender = (int)(MAX_SIM_TIME / NOISE_RESOLUTION);
+	senderNodes.Create(num_of_sender);
+	routerNodes.Create(2);
+	receiverNode.Create(1);
+
+	// Internet stack installation
+	InternetStackHelper internet;
+	internet.Install(senderNodes);
+	internet.Install(routerNodes);
+	internet.Install(receiverNode);
+
+	PointToPointHelper p2p;
+	Ipv4AddressHelper ipv4;
+
+	// Connect routers
+	p2p.SetDeviceAttribute("DataRate", DataRateValue(DataRate(round(LINK_CAPACITY * 1024 * 1024 * 8 / 10)))); //bps
+	p2p.SetChannelAttribute("Delay", StringValue("0ms"));
+	//p2p.SetQueue("ns3::DropTailQueue", "Mode", EnumValue(Queue::QUEUE_MODE_BYTES));
+	//p2p.SetQueue("ns3::DropTailQueue", "Mode", EnumValue(Queue::QUEUE_MODE_PACKETS));
+	//p2p.SetQueue("ns3::DropTailQueue", "MaxPackets", UintegerValue(2));
+	//p2p.SetQueue("ns3::DropTailQueue", "MaxBytes", UintegerValue(550));
+	NetDeviceContainer routerDevs = p2p.Install(routerNodes.Get(0), routerNodes.Get(1));
+
+	// Routers' ip: 208.104.71.0/24 (WAN)
+	ipv4.SetBase(Ipv4Address("208.104.71.0"), "255.255.255.0", "0.0.0.1");
+	ipv4.Assign(routerDevs);
+	tch.Uninstall(routerDevs);
+
+	// Connect sender & receiver with its router
+	CsmaHelper csma;
+	csma.SetChannelAttribute("DataRate", StringValue("100Gbps"));
+	csma.SetChannelAttribute("Delay", StringValue("0ms"));
+	NodeContainer srNodes = senderNodes;
+	srNodes.Add(routerNodes.Get(0));
+	NetDeviceContainer sr_dev = csma.Install(srNodes);
+
+	NetDeviceContainer rr_dev = p2p.Install(receiverNode.Get(0), routerNodes.Get(1));
+
+	// Assign ip addresses to LAN: 208.104.70.0/24 (sender)
+	ipv4.SetBase(Ipv4Address("208.104.70.0"), "255.255.255.0", "0.0.0.1");
+	ipv4.Assign(sr_dev);
+	tch.Uninstall(sr_dev);
+
+	// Assign ip addresses to LAN: 208.104.72.0/24 (receiver)
+	ipv4.SetBase(Ipv4Address("208.104.72.0"), "255.255.255.0", "0.0.0.1");
+	ipv4.Assign(rr_dev);
+	tch.Uninstall(rr_dev);
+
+	///static routing (obsolete)
+	/*
+	// Static routing setting
+	Ipv4StaticRoutingHelper routing;
+
+	// for end systems
+	for (int i = 0; i < num_of_sender; i++) {
+		Ptr<Ipv4StaticRouting> rs;
+		rs = routing.GetStaticRouting(senderNodes.Get(0)->GetObject<Ipv4>());
+		rs->AddNetworkRouteTo(Ipv4Address::GetAny(), Ipv4Mask::GetZero(), 1);
+	}
+	Ptr<Ipv4StaticRouting> rt = routing.GetStaticRouting(receiverNode.Get(0)->GetObject<Ipv4>());
+	rt->AddNetworkRouteTo(Ipv4Address::GetAny(), Ipv4Mask::GetZero(), 1);
+
+	// for routers
+	Ptr<Ipv4StaticRouting> ra = routing.GetStaticRouting(routerNodes.Get(0)->GetObject<Ipv4>());
+	Ptr<Ipv4StaticRouting> rb = routing.GetStaticRouting(routerNodes.Get(1)->GetObject<Ipv4>());
+	// for Router 0
+	ra->AddHostRouteTo(Ipv4Address("208.104.72.1"), routerDevs.Get(0)->GetIfIndex());
+	for (int i = 0; i < num_of_sender; i++) {
+		ostringstream oss;
+		oss.str("");
+		oss << "208.104.70." << i;
+		ra->AddHostRouteTo(Ipv4Address(oss), sr_dev_ptr[i].Get(1)->GetIfIndex());
+	}
+	// for Router 1
+	rb->AddHostRouteTo(Ipv4Address("208.104.72.1"), rr_dev_ptr->Get(1)->GetIfIndex());
+	for (int i = 0; i < num_of_sender; i++) {
+		ostringstream oss;
+		oss.str("");
+		oss << "208.104.70." << i;
+		rb->AddHostRouteTo(Ipv4Address(oss), routerDevs.Get(1)->GetIfIndex());
+	}
+	*/
+
+	//global routing
+	Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+
+	///
+	/// Build traffic
+	///
+
+	// Random variables
+	default_random_engine generator;
+	double mean = TRAFFIC_INTENSITY * 1024 * 1024 / 10;
+	normal_distribution<> trRate(mean, TRAFFIC_VAR * 1024 * 1024 / 10);
+
+	// Sender
+	for (int i = 0; i < num_of_sender; i++) {
+		double temp = max(trRate(generator), 1.0);
+		OnOffHelper onOffHelper("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address("208.104.72.1"), PORT));
+		onOffHelper.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1]"));
+		onOffHelper.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0]"));
+		int rateToSet = round(temp * 8);
+		if (rateToSet < 1) rateToSet = 1;
+		onOffHelper.SetAttribute("DataRate", DataRateValue(DataRate(rateToSet)));
+
+		ApplicationContainer onOffApp = onOffHelper.Install(senderNodes.Get(i));
+		onOffApp.Start(Seconds(i * NOISE_RESOLUTION));
+		onOffApp.Stop(Seconds((i + 1) * NOISE_RESOLUTION));
+	}
+
+	// Receiver
+	PacketSinkHelper sink("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), PORT));
+
+	ApplicationContainer sinkApp = sink.Install(receiverNode.Get(0));
+	sinkApp.Start(Seconds(0));
+	sinkApp.Stop(Seconds(MAX_SIM_TIME + NOISE_RESOLUTION * 1.5));
+
+	p2p.EnablePcap("P2PSim-Receiver", rr_dev.Get(0), true);
+	p2p.EnablePcap("P2PSim-Router2", rr_dev.Get(1), true);
+	p2p.EnablePcap("P2PSim-Router1", routerDevs.Get(0), true);
+	csma.EnablePcap("P2PSim-Sender", sr_dev, true);
+
+	// Simulation start!
+	Simulator::Run();
+	Simulator::Destroy();
+
+	return 0;
+}
---
+++ TestWhiteNoise.cc
@@ -0,0 +1,238 @@
+﻿#include <iostream>
+#include <cmath>
+#include <random>
+#include "ns3/netanim-module.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/internet-apps-module.h"
+#include "ns3/flow-monitor-helper.h"
+#include "ns3/traffic-control-module.h"
+#include "ns3/queue.h"
+
+using namespace std;
+using namespace ns3;
+
+#define ARR_INT 2.0
+#define PORT 10000
+#define FILE_SIZE_DEV 100000
+
+// Tunable parameters:
+#define LINK_CAPACITY 1
+#define TRAFFIC_INTENSITY 1
+#define MAX_SIM_TIME 1.5
+#define NOISE_RESOLUTION 0.1
+#define NOISE_MEAN 
+#define NOISE_VAR 
+
+
+NS_LOG_COMPONENT_DEFINE("TestQueue");
+
+int tot[2];
+int queueSize = 0;
+int cnt = 0;
+int droptimes = 0;
+
+void MacTx(int i, Ptr<const Packet> p) {
+	tot[i]++;
+}
+
+void MarkEnqueue(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > arg
+	,Ptr<const Packet> p) {
+	//cout << "Enqueue!" << endl;
+	queueSize++;
+	cout << "En: " << queueSize << endl;
+	cnt++;
+}
+
+void MarkDequeue(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > arg
+	, Ptr<const Packet> p) {
+	//cout << "Enqueue!" << endl;
+	queueSize--;
+	cout << "De: " << queueSize << endl;
+	//cnt++;
+}
+
+void MarkDrop(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > arg
+	, Ptr<const Packet> p) {
+	droptimes++;
+	cout << "Dr" << endl;
+}
+
+/*
+Ipv4Address ip(char type, int id, int path = 0, int endpoint = 0)
+{
+	int p1, p2, p3;
+	switch (type)
+	{
+	case 'L':
+		p1 = 10;
+		p2 = id / 100;
+		p3 = id % 100;
+		break;
+	case 'S':
+	case 'D':
+		p1 = type == 'S' ? 20 : 30;
+		p2 = id;
+		p3 = path;
+		break;
+	}
+	char ip[50];
+	sprintf(ip, "%d.%d.%d.%d", p1, p2, p3, endpoint);
+	return Ipv4Address(ip);
+}
+*/
+
+int main(void) {
+	tot[0] = tot[1] = 0;
+
+	Time::SetResolution(Time::NS);
+	//LogComponentEnable("OnOffApplication", LOG_LEVEL_INFO);
+	//LogComponentEnable("PacketSink", LOG_LEVEL_INFO);
+
+	TrafficControlHelper tch;
+
+	///
+	/// Build network
+	///
+
+	// Create routers and end systems
+	NodeContainer nodes, routers;
+	nodes.Create(2);
+	routers.Create(2);
+
+	// Internet stack installation
+	InternetStackHelper internet;
+	internet.Install(nodes);
+	internet.Install(routers);
+
+	PointToPointHelper p2p;
+	Ipv4AddressHelper ipv4;
+	
+	// Connect routers
+	p2p.SetDeviceAttribute("DataRate", DataRateValue(DataRate(round(LINK_CAPACITY * 1024 * 1024 * 8 / 10)))); //bps
+	p2p.SetChannelAttribute("Delay", StringValue("0ms"));
+	p2p.SetQueue("ns3::DropTailQueue", "Mode", EnumValue(Queue::QUEUE_MODE_BYTES));
+	//p2p.SetQueue("ns3::DropTailQueue", "Mode", EnumValue(Queue::QUEUE_MODE_PACKETS));
+	p2p.SetQueue("ns3::DropTailQueue", "MaxPackets", UintegerValue(2));
+	p2p.SetQueue("ns3::DropTailQueue", "MaxBytes", UintegerValue(550));
+	NetDeviceContainer e = p2p.Install(routers.Get(0), routers.Get(1));
+
+	// Routers' ip: 208.104.71.0/24 (WAN)
+	ipv4.SetBase(Ipv4Address("208.104.71.0"), "255.255.255.0","0.0.0.1");
+	ipv4.Assign(e);
+	tch.Uninstall(e);
+
+	//Trace e
+	e.Get(0)->TraceConnectWithoutContext("MacTx", MakeBoundCallback(&MacTx, 0));
+
+	// Connect sender with its router
+	p2p.SetDeviceAttribute("DataRate", StringValue("100Gbps"));
+	p2p.SetChannelAttribute("Delay", StringValue("0ms"));
+	NetDeviceContainer es = p2p.Install(nodes.Get(0), routers.Get(0));
+	NetDeviceContainer et = p2p.Install(nodes.Get(1), routers.Get(1));
+
+	// Assign ip addresses to LAN: 208.104.70.0/24 (sender), 208.104.72.0/24 (receiver)
+	ipv4.SetBase(Ipv4Address("208.104.70.0"), "255.255.255.0", "0.0.0.1");
+	ipv4.Assign(es);
+	tch.Uninstall(es);
+
+	ipv4.SetBase(Ipv4Address("208.104.72.0"), "255.255.255.0", "0.0.0.1");
+	ipv4.Assign(et);
+	tch.Uninstall(et);
+
+	// Trace et
+	et.Get(1)->TraceConnectWithoutContext("MacTx", MakeBoundCallback(&MacTx, 1));
+
+	// Static routing setting
+	Ipv4StaticRoutingHelper routing;
+
+	// for end systems
+	Ptr<Ipv4StaticRouting> rs = routing.GetStaticRouting(nodes.Get(0)->GetObject<Ipv4>());
+	Ptr<Ipv4StaticRouting> rt = routing.GetStaticRouting(nodes.Get(1)->GetObject<Ipv4>());
+	rs->AddNetworkRouteTo(Ipv4Address::GetAny(), Ipv4Mask::GetZero(), 1);
+	rt->AddNetworkRouteTo(Ipv4Address::GetAny(), Ipv4Mask::GetZero(), 1);
+
+	// for routers
+	Ptr<Ipv4StaticRouting> ra = routing.GetStaticRouting(routers.Get(0)->GetObject<Ipv4>());
+	Ptr<Ipv4StaticRouting> rb = routing.GetStaticRouting(routers.Get(1)->GetObject<Ipv4>());
+	ra->AddHostRouteTo(Ipv4Address("208.104.70.1"), es.Get(1)->GetIfIndex());
+	rb->AddHostRouteTo(Ipv4Address("208.104.72.1"), et.Get(1)->GetIfIndex());
+	ra->AddHostRouteTo(Ipv4Address("208.104.72.1"), e.Get(0)->GetIfIndex());
+	rb->AddHostRouteTo(Ipv4Address("208.104.70.1"), e.Get(1)->GetIfIndex());
+
+	// Trace Queue
+	ostringstream oss;
+	oss << "/NodeList/"
+		<< routers.Get(0)->GetId()
+		<< "/DeviceList/"
+		<< "*/$ns3::PointToPointNetDevice/TxQueue/Enqueue";
+	Config::Connect(oss.str(), MakeCallback(&MarkEnqueue));
+
+	oss.str("");
+	oss << "/NodeList/"
+		<< routers.Get(0)->GetId()
+		<< "/DeviceList/"
+		<< "*/$ns3::PointToPointNetDevice/TxQueue/Drop";
+	Config::Connect(oss.str(), MakeCallback(&MarkDrop));
+
+	oss.str("");
+	oss << "/NodeList/"
+		<< routers.Get(0)->GetId()
+		<< "/DeviceList/"
+		<< "*/$ns3::PointToPointNetDevice/TxQueue/Dequeue";
+	Config::Connect(oss.str(), MakeCallback(&MarkDequeue));
+
+	///
+	/// Build traffic
+	///
+
+	// Random variables
+	default_random_engine generator;
+	exponential_distribution<> arrival(1 / ARR_INT);
+	double mean = TRAFFIC_INTENSITY * 1024 * 1024 / 10;
+	normal_distribution<> filesize(mean, FILE_SIZE_DEV / 10);
+
+	// Sender
+	double temp = max(filesize(generator), 1.0);
+	OnOffHelper onOffHelper("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address("208.104.72.1"), PORT));
+	onOffHelper.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1]"));
+	onOffHelper.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0]"));
+	int rateToSet = round(temp * 8);
+	if (rateToSet < 1) rateToSet = 1;
+	onOffHelper.SetAttribute("DataRate", DataRateValue(DataRate(rateToSet)));
+
+	ApplicationContainer onOffApp = onOffHelper.Install(nodes.Get(0));
+	onOffApp.Start(Seconds(1.0));
+	onOffApp.Stop(Seconds(MAX_SIM_TIME - 0.1));
+
+	// Receiver
+	PacketSinkHelper sink("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), PORT));
+
+	ApplicationContainer sinkApp = sink.Install(nodes.Get(1));
+	sinkApp.Start(Seconds(0));
+	sinkApp.Stop(Seconds(MAX_SIM_TIME));
+
+	p2p.EnablePcapAll("TestQueue");
+
+	// Simulation start!
+	Simulator::Run();
+	Simulator::Destroy();
+
+	cout << "tot0 = " << tot[0] << ", tot1 = " << tot[1] << endl;
+	cout << "cnt = " << cnt << endl;
+	cout << "Drop times = " << droptimes << endl;
+	cout << routers.Get(0)->GetId() << endl << routers.Get(1)->GetId() << endl;
+
+	ostringstream testo;
+	testo << "Hello";
+	cout << testo.str() << endl;
+	testo.str("");
+	testo << " World";
+	cout << testo.str() << endl;
+	
+
+	return 0;
+}
